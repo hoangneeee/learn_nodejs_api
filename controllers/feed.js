6a2e5325211fd3bb981cdeaf46d0dd8f2157676b
Võ Hoàng <levuthanhtung11@gmail.com>
2022-03-06 23:34:17 +0700

    xử lý hình ảnh và xử lý lỗi

--- controllers/feed.js
+++ controllers/feed.js
@@ -20,7 +20,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({ message: 'Validation failed', errors: errors.array()})
+        const error = new Error('Validation failed');
+        error.statusCode = 442;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -38,7 +40,10 @@ exports.createPost = (req, res, next) => {
         post: result
     });
     }).catch(err => {
-        console.log(err);
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        throw err;
     });
 
 };
