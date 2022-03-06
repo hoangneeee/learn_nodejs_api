bd5b36f4784c4e3077b852450dfba631959f56d2
Võ Hoàng <levuthanhtung11@gmail.com>
2022-03-06 14:58:24 +0700

    learn

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,15 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: 'First Post', content: 'This is the first post'}]
+        posts: [{
+            _id: '1',
+            title: 'First Post', 
+            content: 'This is the first post', 
+            imagesUrl: 'images/    django.png',
+            creator: {
+                name: 'Hoang'
+            },
+            createdAt: new Date()
+        }]
     });
 };
 
